b5229e1a471be5ab32b900f4a61a095c44dc85d6
luiscoro <luiscoronel_97@hotmail.com>
2021-08-28 23:39:58 -0500

    v1.0

--- src/main/java/com/comercial/acat/service/CarruselService.java
+++ src/main/java/com/comercial/acat/service/CarruselService.java
@@ -1,7 +1,7 @@
 package com.comercial.acat.service;
 
 import java.util.List;
-
+//servicio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
